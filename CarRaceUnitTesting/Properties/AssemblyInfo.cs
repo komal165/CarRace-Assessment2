919ecee78f414e4a953173000bac025be1075cef
komal <komal@LAPTOP-GPSG7DIK>
2019-08-19 11:24:21 +1200

    unit test

---
+++ CarRaceUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CarRaceUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CarRaceUnitTesting")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("90cf7248-e390-416f-9deb-23eb9fb64603")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CarRaceUnitTesting/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CarRace;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarRaceUnitTest
+{
+    [TestClass]
+    public class CarRaceUnitTest
+    {
+        [TestMethod]
+        public void PayOutTest()
+        {
+            // Arrange
+            var bet = new Bet();
+
+            // Act
+            var actual = bet.PayOut(0);
+
+            // Assert
+            Assert.AreEqual(0, actual);
+
+        }
+
+        [TestMethod]
+        public void Error_Message_Due_To_Not_Enough_Cash()
+        {
+            // Arrange
+            var guy = new Bettor();
+
+            // Act
+            var actual = guy.PlaceBid(100, 0);
+
+            // Assert
+            Assert.AreEqual(false, actual);
+        }
+    }
+}
